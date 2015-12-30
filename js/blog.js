41d56a2372e3381c48636dc490d64a00d4cab776
Zach Drago <zachdrago@MacBook-Pro-3.local>
2015-12-30 01:02:05 -0800

    fixes- functioning

--- js/blog.js
+++ js/blog.js
@@ -1,22 +1,32 @@
 $(function() {
- 
-    Parse.$ = jQuery;
- 
-    // Replace this line with the one on your Quickstart Guide Page
-    Parse.initialize("sbKiF5BGV13YZfTm9tzxM4SCepj9gHaQ3vFWcEsi", "UQ8G4kH6FXy0n4K43sUR3DlFlZ65WNvKHQr2XsET");
- 
-    var Blog = Parse.Object.extend("Blog");
-    var Blogs = Parse.Collection.extend({
-    model: Blog,
-});
-    var blogs = new Blogs();
-    blogs.fetch({
-    success: function(blogs) {
-        console.log(blogs);
-    },
-    error: function(blogs, error) {
-        console.log(error);
-    }
-});
- 
+
+	Parse.$ = jQuery;
+
+	// Replace this line with the one on your Quickstart Guide Page
+	Parse.initialize("sbKiF5BGV13YZfTm9tzxM4SCepj9gHaQ3vFWcEsi", "UQ8G4kH6FXy0n4K43sUR3DlFlZ65WNvKHQr2XsET");
+
+	var Blog = Parse.Object.extend("Blog"),
+		Blogs = Parse.Collection.extend({
+			model: Blog
+		}),
+		BlogsView = Parse.View.extend({
+			template: Handlebars.compile($('#blogs-tpl').html()),
+			render: function() { 
+				var collection = { blog: this.collection.toJSON() };
+				this.$el.html(this.template(collection));
+			}
+		});
+		blogs = new Blogs();
+
+		blogs.fetch({
+			success: function(blogs) {
+				var blogsView = new BlogsView({ collection: blogs });
+				blogsView.render();
+				$('.main-container').html(blogsView.el);
+			},
+			error: function(blogs, error) {
+				console.log(error);
+			}
+		});
+
 });
