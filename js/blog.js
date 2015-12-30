7c829d98ffe311557c9e8a8fcf601f9429f9deca
Zach Drago <zachdrago@MacBook-Pro-3.local>
2015-12-29 23:48:20 -0800

    Fetch Blogs

--- js/blog.js
+++ js/blog.js
@@ -5,10 +5,18 @@ $(function() {
     // Replace this line with the one on your Quickstart Guide Page
     Parse.initialize("sbKiF5BGV13YZfTm9tzxM4SCepj9gHaQ3vFWcEsi", "UQ8G4kH6FXy0n4K43sUR3DlFlZ65WNvKHQr2XsET");
  
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
+    var Blog = Parse.Object.extend("Blog");
+    var Blogs = Parse.Collection.extend({
+    model: Blog
+});
+    var blogs = new Blogs();
+    blogs.fetch({
+    success: function(blogs) {
+        console.log(blogs);
+    },
+    error: function(blogs, error) {
+        console.log(error);
+    }
+});
  
 });
